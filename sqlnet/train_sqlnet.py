7af3e48ce02831bf8aa6f9fc793d562519275ca1
pc-bilal <rachik.bilal@gmail.com>
2019-06-03 18:25:39 +0200

    =modif

--- sqlnet/train_sqlnet.py
+++ sqlnet/train_sqlnet.py
@@ -61,12 +61,12 @@ for i in range(100):
     if val_acc[0]> best_cond_acc:
         best_cond_acc = val_acc[0]
         best_cond_idx = i+1
-        torch.save(model_sqlnet_cond.cond_pred.state_dict(),
-                        'saved_model/epoch%d.cond_model%s'%(i+1,cond_m))
+        #torch.save(model_sqlnet_cond.cond_pred.state_dict(),
+         #               'saved_model/epoch%d.cond_model%s'%(i+1,cond_m))
         torch.save(model_sqlnet_cond.cond_pred.state_dict(), cond_m)
                     
-        torch.save(model_sqlnet_cond.cond_embed_layer.state_dict(),
-                        'saved_model/epoch%d.cond_embed%s'%(i+1,cond_e))
+        #torch.save(model_sqlnet_cond.cond_embed_layer.state_dict(),
+         #               'saved_model/epoch%d.cond_embed%s'%(i+1,cond_e))
         torch.save(model_sqlnet_cond.cond_embed_layer.state_dict(), cond_e)
             
     print (' Best val acc = %s, on epoch %s individually'%(
