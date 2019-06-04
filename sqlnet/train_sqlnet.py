bab70ca6883a92a690e8763d7d45a45afaf3364d
rachikbilal <brachik@bigapps.fr>
2019-06-04 11:08:19 +0200

    =modif

--- sqlnet/train_sqlnet.py
+++ sqlnet/train_sqlnet.py
@@ -24,6 +24,13 @@ word_emb = load_word_emb('glove/glove.42B.300d.txt', \
 model_sqlnet_cond= SQLNet_cond(word_emb, N_word=N_word, use_ca=True,
                 gpu=GPU,trainable_emb =True)
 
+cond_e="sqlnet_emb_cond"
+cond_m="sqlnet_cond"
+
+
+model_sqlnet_cond.cond_pred.load_state_dict(torch.load(cond_m))
+model_sqlnet_cond.cond_embed_layer.load_state_dict(torch.load(cond_e))
+
 optimizer = torch.optim.Adam(model_sqlnet_cond.parameters(),
             lr=learning_rate, weight_decay = 0)
 
@@ -32,18 +39,16 @@ init_acc = epoch_acc_sqlnet(model_sqlnet_cond, BATCH_SIZE,
                 val_sql_data, val_table_data)
 
 
+
 best_cond_acc = init_acc[0]
 best_cond_idx = 0
-
-cond_e="sqlnet_emb_cond"
-cond_m="sqlnet_cond"
 
 
 print ('Init dev acc_qm: %s\n  breakdown on (where): %s'%\
                 init_acc)
 torch.save(model_sqlnet_cond.cond_pred.state_dict(), cond_e)
            
-for i in range(100):
+for i in range(20,100):
     print ('Epoch %d @ %s'%(i+1, datetime.datetime.now()))
     print (' Loss = %s'%epoch_train_sqlnet(
             model_sqlnet_cond,optimizer, BATCH_SIZE, 
